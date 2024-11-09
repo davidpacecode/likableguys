171c664293fb626c47e51020216560d9a99f08be
davidpacecode <david.pace@gmail.com>
2024-11-09 11:11:37 -0600

    added first two races of Surge Nov 9 2024

--- plugins/builders/helpers.rb
+++ plugins/builders/helpers.rb
@@ -1,10 +1,71 @@
 class Builders::Helpers < SiteBuilder
 
   def build
-    helper :add_two_nums do |num_1, num_2|
-      num_1 + num_2
+    helper :get_motivational_times do |age, gender, age_type, distance, stroke, event_type|
+
+      member = site.data.motivational_times.find do |m|
+        m.age == age &&
+        m.gender == gender &&
+        m.age_type == age_type &&
+        m.distance == distance &&
+        m.stroke == stroke &&
+        m.event_type == event_type
+      end
+
+      member ? [member.aaaa, member.aaa, member.aa, member.a, member.bb, member.b] : nil
+
     end
+
+    helper :convert_to_seconds do |time|
+
+      if time.include? ":"
+        mins,secs = time.split(":")
+        time = mins.to_f * 60 + secs.to_f
+      else
+        time.to_f
+      end
+
+    end
+
+    helper :get_grade do |age, gender, age_type, distance, stroke, event_type, time|
+
+      times = helpers.get_motivational_times(age, gender, age_type, distance, stroke, event_type)
+     
+      if times.nil?
+        return nil
+      end
+
+      aaaa = helpers.convert_to_seconds(times[0])
+      aaa = helpers.convert_to_seconds(times[1])
+      aa = helpers.convert_to_seconds(times[2])
+      a = helpers.convert_to_seconds(times[3])
+      bb = helpers.convert_to_seconds(times[4])
+      b = helpers.convert_to_seconds(times[5])
+
+      case helpers.convert_to_seconds(time)
+      when 0..aaaa
+        grade = "AAAA"
+      when aaaa..aaa
+        grade = "AAA"
+      when aaa..aa
+        grade = "AA"
+      when aa..a
+        grade = "A"
+      when a..bb
+        grade = "BB"
+      when bb..b
+        grade = "B"
+      else
+        grade = "C"
+      end
+
+      grade
+
+    end
+
   end
+
+
 end
 
 
--- src/index.md
+++ src/index.md
@@ -10,6 +10,8 @@ layout: default
 [2024 Surge Cornucopia Classic](https://www.gomotionapp.com/stssrg/UserFiles/Image/QuickUpload/saturday-heat-sheet_039988.pdf)
 <br />
 All races are Short Course and all distances are Yards
+<br />
+[Live results](https://swimnerd.live/score?id=1734)
 
 <div class="table-container">
   <table class="table">
@@ -36,11 +38,11 @@ All races are Short Course and all distances are Yards
         <td>2</td>
         <td>5</td>
         <td>27.96</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><sl-badge variant="neutral" pill pulse>{{get_grade("12","boys","single_age","50","fr","scy","27.96")}}</sl-badge></td>
+        <td>25.93</td>
+        <td><sl-badge variant="success" pill pulse>{{get_grade("12","boys","single_age","50","fr","scy","25.93")}}</sl-badge></td>
+        <td>2nd</td>
+        <td><a href="https://youtu.be/3GtnsaOdNvg">Link to race</a></td>
       </tr>
       <tr>
         <td>12</td>
@@ -49,11 +51,11 @@ All races are Short Course and all distances are Yards
         <td>1</td>
         <td>5</td>
         <td>36.47</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><sl-badge variant="neutral" pill pulse>{{get_grade("12","boys","single_age","50","br","scy","36.47")}}</sl-badge></td>
+        <td>36.32</td>
+        <td><sl-badge variant="success" pill pulse>{{get_grade("12","boys","single_age","50","br","scy","36.32")}}</sl-badge></td>
+        <td>3rd</td>
+        <td><a href="https://youtu.be/akmvvUInwsI">Link to race</a></td>
       </tr>
       <tr>
         <td>18</td>

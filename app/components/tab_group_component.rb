54e87c3c6c9c3df5d84eb7be888789481af5378d
davidpacecode <david.pace@gmail.com>
2025-04-08 23:56:22 -0500

    I almost have the time browser working...one more view component

---
+++ app/components/tab_group_component.rb
@@ -0,0 +1,7 @@
+class TabGroupComponent < ViewComponent::Base
+  def initialize(standard_type:, course:, time_standards:)
+    @standard_type = standard_type
+    @course = course
+    @time_standards = time_standards
+  end
+end

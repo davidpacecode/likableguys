c9cc531598eedef182f5a1021b7e51916ad0e921
davidpacecode <davidpace@gmail.com>
2025-04-10 00:15:39 -0500

    i think i have the mvp of the time browser done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def format_time_standard(time)
+    if time.to_f > 60.0
+      minutes = time.to_i / 60
+      seconds = time.to_i % 60
+      hundredths = ((time.to_f - time.to_i) * 100).round
+      format("%d:%02d.%02d", minutes, seconds, hundredths)
+    else
+      time
+    end
+  end
 end

8115a5263c3200cc5336cad8e86df7fb432131e4
davidpacecode <davidpace@gmail.com>
2025-04-19 16:39:16 -0500

    tags time addition to best times is just about done

--- app/components/tags_time_component.rb
+++ app/components/tags_time_component.rb
@@ -4,14 +4,13 @@ class TagsTimeComponent < ViewComponent::Base
   def initialize(gender:, age:, distance:, stroke:, course:, time_type:)
     @gender = gender
     @age = age.to_i
-    @distance = distance
+    @distance = distance.to_i
     @stroke = stroke
     @course = course
     @time_type = time_type
 
-    @tags_time_cut = TagsTimeCut.find_by("gender = ? and age_group = ?
-      and distance = ? and stroke = ? and course = ?", @gender,
-      age_group_for(@age), @distance, @stroke, @course)
+    @tags_time_cut = TagsTimeCut.find_by("gender = ? and age_group = ? and distance = ? and stroke = ? and course = ?",
+      @gender, age_group_for(@age), @distance, @stroke, @course)
   end
 
   def age_group_for(age)

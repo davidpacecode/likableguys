466635e96b5f8d0739f7493ebe557ae86ae685ed
davidpacecode <davidpace@gmail.com>
2025-04-20 19:18:27 -0500

    have best times mostly working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ module ApplicationHelper
       time
     end
   end
+
+  # Calculate the CMC's age
+  def cmc_age
+    render(CmcAgeComponent.new)
+  end
+
+  # get the TAGS time for a set of parameters. It's possible
+  # this should be less broadly scoped but leaving here
+  # for now since I think it's needed in Races, SwimMeets, and
+  # maybe elsewhere
+  def tags_time(gender:, age:, distance:, stroke:, course:, time_type:)
+    render(
+      TagsTimeComponent.new(
+        gender: gender,
+        age: age,
+        distance: distance,
+        stroke: stroke,
+        course: course,
+        time_type: time_type
+      )
+    )
+  end
 end

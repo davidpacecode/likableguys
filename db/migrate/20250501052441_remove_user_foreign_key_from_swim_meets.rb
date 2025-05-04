a4809d91a32c3fa3bc6939ae95f8b4681b724fd6
davidpacecode <davidpace@gmail.com>
2025-05-03 21:03:34 -0500

    deleted what i think are superfluous migrations and also editing home page

--- db/migrate/20250501052441_remove_user_foreign_key_from_swim_meets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserForeignKeyFromSwimMeets < ActiveRecord::Migration[8.0]
-  def change
-    remove_foreign_key :swim_meets, :users
-  end
-end
--- db/migrate/20250502043622_remove_not_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNotNullConstraint < ActiveRecord::Migration[8.0]
-  def change
-    change_column_null :swim_meets, :user_id, true
-  end
-end
